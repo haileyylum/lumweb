b18edb1a62855ddcbcdd1a443126cb0f9439e9a5
hailey.lum <haileyy.lum@gmail.com>
2020-03-02 14:28:16 +0800

    mobile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import Home from "./Screens/Home/index"
-
+import React from "react";
+import logo from "./logo.svg";
+import "whatwg-fetch";
+import "./App.css";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Home from "./Screens/Home/index";
 
 function App() {
   return (
     <div className="App">
       <div className="site-name">
-      <h3>Wai lum</h3>
-
+        <h3>Wai lum</h3>
       </div>
-       <Router>
-            <div>
-              <Route path="/" component={Home} />
-            </div>
-          </Router>
+      <Router>
+        <div>
+          <Route path="/" component={Home} />
+        </div>
+      </Router>
     </div>
   );
 }
